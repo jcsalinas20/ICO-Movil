3aedaee2f753dcb62aff99a09504c5ea6cfddf92
jcsalinas20 <jcsalinasn20@gmail.com>
2020-03-05 23:19:51 +0100

    slide touch ya funciona

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,31 +16,84 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+$(document).ready(function () {
+    $("#submit").click(authLogin)
+
+    $('.tabs').tabs()
+})
+
+function authLogin() {
+    var user = document.getElementById("user").value
+    getEncryptPass(user, document.getElementById("pass").value)
+}
+
+function getEncryptPass(user, pass) {
+    $.ajax({
+            method: "GET",
+            url: "https://api-ico.herokuapp.com/api/encrypt/" + pass,
+            dataType: "json" // necessitem això pq ens retorni un objecte JSON
+        })
+        .done(function (msg) {
+            console.log(msg.password)
+            login(user, msg.password)
+        })
+        .fail(function () {
+            alert("ERROR")
+        })
+}
+
+function login(user, pass) {
+    $.ajax({
+            method: "GET",
+            url: "https://api-ico.herokuapp.com/api/paciente/auth/" +
+                user +
+                "/" +
+                pass,
+            dataType: "json" // necessitem això pq ens retorni un objecte JSON
+        })
+        .done(function (msg) {
+            M.toast({
+                html: msg.mensaje
+            })
+            if (msg.mensaje === "El login se realizó correctamente.") {
+                location.href = "./pages/home/home.html"
+            }
+        })
+        .fail(function () {
+            alert("ERROR")
+        })
+}
+
 var app = {
     // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    initialize: function () {
+        document.addEventListener(
+            "deviceready",
+            this.onDeviceReady.bind(this),
+            false
+        )
     },
 
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+    onDeviceReady: function () {
+        this.receivedEvent("deviceready")
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id)
+        var listeningElement = parentElement.querySelector(".listening")
+        var receivedElement = parentElement.querySelector(".received")
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute("style", "display:none;")
+        receivedElement.setAttribute("style", "display:block;")
 
-        console.log('Received Event: ' + id);
+        console.log("Received Event: " + id)
     }
-};
+}
 
-app.initialize();
+app.initialize()
