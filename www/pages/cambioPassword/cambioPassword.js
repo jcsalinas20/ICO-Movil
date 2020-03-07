14cf4d7da9164a77e7618fb4b4f212c897086e50
jcsalinas20 <jcsalinasn20@gmail.com>
2020-03-07 12:12:27 +0100

    control de contraseñas

--- www/pages/cambioPassword/cambioPassword.js
+++ www/pages/cambioPassword/cambioPassword.js
@@ -7,13 +7,21 @@ function mostrarError(text) {
     document.getElementById("error").innerHTML = text
 }
 
+function quitarEspacios(pass, pass2) {
+    pass.value = pass.value.replace(" ", "")
+    pass2.value = pass2.value.replace(" ", "")
+}
+
 function comprobarPassword() {
     var pass = document.getElementById("pass")
     var passRepetida = document.getElementById("passRepetida")
+
+    quitarEspacios(pass, passRepetida)
+
     if (pass.value.length > 5) {
         if (pass.value === passRepetida.value) {
             console.log(pass.value + " - " + passRepetida.value)
-        }else {
+        } else {
             mostrarError("Las contraseñas no coinciden")
             passRepetida.value = ""
         }
