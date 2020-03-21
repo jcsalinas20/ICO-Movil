c4c3abc7e924f38076be61b65e3f4969c274a289
jcsalinas20 <jcsalinasn20@gmail.com>
2020-03-21 16:04:54 +0100

    botón cerrar sesion terminado

--- www/pages/perfil/perfil.js
+++ www/pages/perfil/perfil.js
@@ -7,3 +7,25 @@ function cambiarContra() {
     window.parent.document.getElementById('framePerfil').style.display = 'none'
     window.parent.document.getElementById('container-frame-cambio-password').style.display = 'flex'
 }
+
+function cerrarSesion() {
+    window.parent.document.getElementById("frame-preload").contentWindow.document.getElementById('mensaje').innerHTML = 'Cerrando sesión.'
+    window.parent.document.getElementById('frame-preload').style.display = 'flex'
+    var dni = document.getElementById('dni').innerHTML.split(':</b> ')
+    $.ajax({
+        type: "GET",
+        url: `https://api-ico.herokuapp.com/api/${dni[1]}/logout`,
+        crossDomain: true,
+        dataType: "json"
+    })
+        .done(async function(res) {
+            if (res) {
+                window.parent.document.getElementById('frame-preload').style.display = 'none'
+                window.parent.location.href = './../../'
+            }
+        })
+        .fail(function() {
+            // LLAMAR AL METODO DEL PADRE
+            activateToast("No se pudo establecer conexión con el servidor")
+        })
+}
